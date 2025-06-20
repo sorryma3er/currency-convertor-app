583a31a9d8dcfb2a374ff6d4eec3b7817b8b9393
sorryma3er <whc0713air@gmail.com>
2025-06-20 11:16:12 +0700

    Defined basic const colors based on Figma UI design.

---
+++ lib/constraints.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFFE0BABA);
+const kPinkCardColor = Color(0xFFA97070);
+const kBlueCardColor = Color(0xFFE39666);
+const kButtonColor = Color(0xFFE39666);
+const kDarkTextColor = Color(0xFF000000);
+
